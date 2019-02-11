7defbbefd43e02240a6a1473359fa2d40a1faf8f
Andrew Van Wormer <a.vwormer@gmail.com>
2019-02-11 13:43:34 -0800

    Complete formatting amenities into a two-column table

--- database/index.js
+++ database/index.js
@@ -4,16 +4,16 @@ const mysqlConfig = require('./config.js');
 const connection = mysql.createConnection(mysqlConfig);
 
 const getAmenenities = function(serverCallback) {
-  let query = 'SELECT * FROM amenities';
+  let query = 'SELECT * FROM amenities ORDER BY appeal DESC';
   let sqlCb = (err, dbData) => {
     if (err) {
       throw err;
       return
     } else {
-      serverCallback(null, dbData)
+      serverCallback(null, dbData);
     }
   }
-  connection.query(query, sqlCb)
+  connection.query(query, sqlCb);
 }
 
 module.exports = {
